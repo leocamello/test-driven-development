4ed4e851136b0aa2243534e4e18db7a62d7d0e22
Leonardo Nascimento <leocamello@gmail.com>
2019-06-12 01:34:30 +0100

    Initial setup for unit testing

---
+++ embedded-c/tests/leddrivertests.c
@@ -0,0 +1,11 @@
+#include "unity_fixture.h"
+
+TEST_GROUP(LedDriver);
+
+TEST_SETUP(LedDriver)
+{
+}
+
+TEST_TEAR_DOWN(LedDriver)
+{
+}
---
+++ embedded-c/tests/testrunner.c
@@ -0,0 +1,5 @@
+#include "unity_fixture.h"
+
+TEST_GROUP_RUNNER(LedDriver)
+{
+}
