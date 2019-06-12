ffefacb353713ac425dbc1294c87020d97825f07
Leonardo Nascimento <leocamello@gmail.com>
2019-06-12 22:41:36 +0100

    Led driver turn off led one passing test

--- embedded-c/include/leddriver.h
+++ embedded-c/include/leddriver.h
@@ -2,3 +2,4 @@
 
 void LedDriver_Initialize(uint16_t* address);
 void LedDriver_TurnOn(int ledNumber); 
+void LedDriver_TurnOff(int ledNumber); 
--- embedded-c/src/leddriver.c
+++ embedded-c/src/leddriver.c
@@ -12,3 +12,8 @@ void LedDriver_TurnOn(int ledNumber)
 {
     *ledsAddress = 1;
 }
+
+void LedDriver_TurnOff(int ledNumber)
+{
+    *ledsAddress = 0;
+}
