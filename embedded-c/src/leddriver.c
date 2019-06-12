3c7adae4c0b066138fd6a98e94d6005ba865fadc
Leonardo Nascimento <leocamello@gmail.com>
2019-06-12 22:58:18 +0100

    Led driver turn on multiple leds passing test

--- embedded-c/src/leddriver.c
+++ embedded-c/src/leddriver.c
@@ -10,7 +10,7 @@ void LedDriver_Initialize(uint16_t* address)
 
 void LedDriver_TurnOn(int ledNumber)
 {
-    *ledsAddress = 1;
+    *ledsAddress |= 1 << (ledNumber - 1);
 }
 
 void LedDriver_TurnOff(int ledNumber)
