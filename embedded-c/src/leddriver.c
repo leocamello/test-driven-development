0577760d49adb65d6f3793e842023b17491ea3a1
Leonardo Nascimento <leocamello@gmail.com>
2019-06-18 23:05:21 +0100

    Led driver refactoring

--- embedded-c/src/leddriver.c
+++ embedded-c/src/leddriver.c
@@ -5,6 +5,11 @@ enum { ALL_LEDS_OFF = 0, ALL_LEDS_ON = ~ALL_LEDS_OFF };
 static uint16_t ledsImage;
 static uint16_t* ledsAddress;
 
+static void UpdateHardware(void)
+{
+    *ledsAddress = ledsImage;    
+}
+
 static int ConvertLedNumberToBit(int ledNumber)
 {
     return 1 << (ledNumber - 1);
@@ -14,23 +19,23 @@ void LedDriver_Initialize(uint16_t* address)
 {
     ledsAddress = address;
     ledsImage = ALL_LEDS_OFF;
-    *ledsAddress = ledsImage;
+    UpdateHardware();
 }
 
 void LedDriver_TurnOn(int ledNumber)
 {
     ledsImage |= ConvertLedNumberToBit(ledNumber);
-    *ledsAddress = ledsImage;
+    UpdateHardware();
 }
 
 void LedDriver_TurnOff(int ledNumber)
 {
     ledsImage &= ~ConvertLedNumberToBit(ledNumber);
-    *ledsAddress = ledsImage;
+    UpdateHardware();
 }
 
 void LedDriver_TurnAllOn(void)
 {
     ledsImage = ALL_LEDS_ON;
-    *ledsAddress = ledsImage;
+    UpdateHardware();
 } 
