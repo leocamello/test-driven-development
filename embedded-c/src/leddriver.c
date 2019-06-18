9985926f082a699395602f34bfedeefe5a2d46c6
Leonardo Nascimento <leocamello@gmail.com>
2019-06-18 22:59:23 +0100

    Led driver do not read from memory passing test

--- embedded-c/src/leddriver.c
+++ embedded-c/src/leddriver.c
@@ -2,6 +2,7 @@
 
 enum { ALL_LEDS_OFF = 0, ALL_LEDS_ON = ~ALL_LEDS_OFF };
 
+static uint16_t ledsImage;
 static uint16_t* ledsAddress;
 
 static int ConvertLedNumberToBit(int ledNumber)
@@ -12,20 +13,24 @@ static int ConvertLedNumberToBit(int ledNumber)
 void LedDriver_Initialize(uint16_t* address)
 {
     ledsAddress = address;
-    *ledsAddress = ALL_LEDS_OFF;
+    ledsImage = ALL_LEDS_OFF;
+    *ledsAddress = ledsImage;
 }
 
 void LedDriver_TurnOn(int ledNumber)
 {
-    *ledsAddress |= ConvertLedNumberToBit(ledNumber);
+    ledsImage |= ConvertLedNumberToBit(ledNumber);
+    *ledsAddress = ledsImage;
 }
 
 void LedDriver_TurnOff(int ledNumber)
 {
-    *ledsAddress &= ~ConvertLedNumberToBit(ledNumber);
+    ledsImage &= ~ConvertLedNumberToBit(ledNumber);
+    *ledsAddress = ledsImage;
 }
 
 void LedDriver_TurnAllOn(void)
 {
-    *ledsAddress = ALL_LEDS_ON;
+    ledsImage = ALL_LEDS_ON;
+    *ledsAddress = ledsImage;
 } 
